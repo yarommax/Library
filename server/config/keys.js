4d1bec7db7b536653a7064e7d09e93657f829b85
Maxim <yarommax@yandex.ru>
2019-02-13 14:38:26 +0300

    Added token support and modified controllers

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGO_URL: 'mongodb://localhost:27017/myapp'
+    MONGO_URL: 'mongodb://localhost:27017/library',
+    jwt: 'dev-jwt'
 }
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,14 +1,64 @@
-module.exports.login = function(req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const keys = require('../config/keys');
+
+module.exports.login = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email});
+    if(candidate) {
+        //email совпадает > делаем проверку пароля
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if(passwordResult) {
+            //генерация токена. пароли совпали
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60*60*2});
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            });
+
+        } else {
+            res.status(401).json({
+                error: 'Пароли не совпадают.'
+            });
         }
-    })
+    } else {
+        //email не найден  > выкидываем ошибку.
+        res.status(404).json({
+            error: 'Пользователь с таким email не найден.'
+        });
+    }
 }
 
-module.exports.register = function (req, res) {
-    res.status(200).json({
-        register: 'register'
-    })
+
+
+module.exports.register = async function (req, res) {
+
+    //существует ли пользователь с таким email    
+    const candidate = await User.findOne({email: req.body.email});
+
+    if (candidate) {
+        //совпадение найдено - вывести ошибку
+        res.status(409).json({
+            error: 'Email занят. Попробуйте другой.'
+        });
+    } else {
+        //совпадений нет - можно регистрировать
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password,salt)
+        });
+
+        try {
+            await user.save();
+            res.status(201).json(user);
+        } catch(e) {
+            //обработать ошибку
+        }        
+    }
 }
