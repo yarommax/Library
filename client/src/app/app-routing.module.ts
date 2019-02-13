58a798e17e6c8cb3f4df58f18ca05458ed261cad
Maxim <yarommax@yandex.ru>
2019-02-14 02:58:30 +0300

    Created layouts for Auth and Site

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+
+const routes: Routes = [
+    {
+        path: '', component: AuthLayoutComponent, children: [
+            {path: 'login', component: LoginPageComponent}
+        ]
+    },
+    {
+        path: '', component: SiteLayoutComponent, children: [
+            
+        ]
+    }
+]
+
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule {
+
+}
